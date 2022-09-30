1c2a82535845367226db7ccf6d211a345be3da17
Alpmon <110713912+Alpmon@users.noreply.github.com>
2022-09-30 19:15:58 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,4 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alpmon/Taiden/Loader/"..({
-  [994732206] = "BloxFruits",  
-  [286090429] = "Arsenal",
-  [4954752502] = "TowerofMisery",
+  [994732206] = "BF",  
 })[game.GameId]))()
