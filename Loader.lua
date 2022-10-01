8025f4e78bf3b24b7ff983e6b2b816edf9041f4a
Alpmon <110713912+Alpmon@users.noreply.github.com>
2022-10-01 18:00:11 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alpmon/Taiden/Loader/"..({
   [994732206] = "BF",  
-  [978370137 or 1730877806]
+  [978370137 or 1730877806] = "GPO",
 })[game.GameId]))()
