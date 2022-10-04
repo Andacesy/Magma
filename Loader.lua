f613418b4dea29ffeade49bd3143e6f9ae54ede3
Alpmon <110713912+Alpmon@users.noreply.github.com>
2022-10-04 19:25:46 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Alpmon/Taiden/Loader/"..({
-  [994732206] = "BF",  
-  [978370137 or 1730877806] = "GPO",
+  [994732206] = "BloxFruit",  
+  [978370137 or 1730877806] = "Grand Piece Online",
 })[game.GameId]))()
