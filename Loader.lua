8819c115830a402a1dc1bf7d6d2cc9254540ff15
Alpmon <110713912+Alpmon@users.noreply.github.com>
2022-10-08 22:00:12 +0700

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,7 +0,0 @@
-repeat wait() until game:IsLoaded()
-local PlaceId = game.PlaceId
-if PlaceId == 994732206 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Alpmon/Taiden/Loader/BloxFruit")()
-elseif PlaceId == 978370137 or 1730877806 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Alpmon/Taiden/Loader/Grand%20Piece%20Online")()
-end
