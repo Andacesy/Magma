04d986510bf1a876ebb85fed233c043ae0aa899c
Alpmon <110713912+Alpmon@users.noreply.github.com>
2022-10-21 17:21:23 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,4 +4,6 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Alpmon/Taiden/Loader/BloxFruits")()
 elseif PlaceId == 914010731 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Alpmon/Taiden/Loader/RoGhoul")()
+elseif PlaceId == 6366283085 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Alpmon/Taiden/Loader/Freefire.lua")()
 end
